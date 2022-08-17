4d070adfe8b309bef6b6555af3a4456625401705
davidnene <davienesh4@gmail.com>
2022-08-17 13:47:35 +0300

    commit tree 515062e8ec6af43b61324db058d01df03a475a09

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require "pry"
 
+def array
+    ["fiddleheads", "okra", "kohlrabi"]
 end
+
+def oxford_comma(array)
+    if array.length > 1 and array.length < 3
+        arr_1 = array.pop
+        array.push("and", "#{arr_1}")
+        new_str1 = array.join(" ")
+        new_str1
+    elsif array.length > 2
+        arr_2 = array.pop
+        array.push("and")
+        new_str2 = array.join(", ").insert(-1, " #{arr_2}")
+        new_str2   
+    else
+        array.join("")
+    end
+end
+
+oxford_comma(array)
